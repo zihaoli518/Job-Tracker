8c7c8565fa375bd522accbcc4d48e201b50ccaad
zihaoli518 <zihaoli518@gmail.com>
2023-02-08 23:48:09 -0800

    adding front end template

---
+++ client/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+
+
+//create a root
+const root = createRoot(document.getElementById('root'));
+
+
+//initial render
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
